5ef132393a64850c402a492722734c3279817693
Rasel Ahmed <wp@perfectpointmarketing.com>
2023-01-01 20:14:30 +0600

    Initial commit

---
+++ components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="bg-blue-500">Test</button>
+</template>
+
+<script>
+export default {
+  name: "Button"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Home</h2>
+  <Button/>
+</template>
+
+<script>
+
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
